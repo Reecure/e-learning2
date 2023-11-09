a342a5620b3bf4b68116e670787016b17693054b
Yura <ogogan45@gmail.com>
2023-11-10 00:08:14 +0200

    update news item

--- src/shared/ui/profile/ui/News/NewsItem/NewsItem.tsx
+++ src/shared/ui/profile/ui/News/NewsItem/NewsItem.tsx
@@ -1,5 +1,7 @@
-import {FC} from "react";
+import React, {FC, useState} from "react";
 import {News} from "@/enteties/News/model/types/module";
+import {Button, ButtonThemes} from "@/shared/ui";
+import {AiOutlineDown} from "react-icons/ai";
 
 interface Props {
     className?: string;
@@ -8,9 +10,32 @@ interface Props {
 
 const NewsItem: FC<Props> = ({className, item}) => {
 
+    const [descriptionOpen, setDescriptionOpen] = useState(false);
+
+    const opendescriptionHandler = () => {
+        setDescriptionOpen(prev => !prev);
+    };
+
     return (
         <div className={`${className} w-full rounded-md px-2 py-4 mb-2`}>
-            <p>{item.title}</p>
+            <div className={"flex justify-between items-center "}>
+                <p className={"text-xl font-medium "}>{item.title}</p>
+                <Button
+                    type={"submit"}
+                    className={`!p-1 sm:!p-2 !rounded-md`}
+                    theme={ButtonThemes.TEXT}
+                    onClick={opendescriptionHandler}
+                >
+                    <>
+                        <AiOutlineDown/>
+                    </>
+                </Button>
+            </div>
+            {
+                descriptionOpen && <div>
+                    <p className={"text-neutral-300 text-md mt-3"}>{item.description}</p>
+                </div>
+            }
         </div>
     );
 };
